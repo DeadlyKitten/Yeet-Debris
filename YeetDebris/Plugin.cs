f02ff7ecdae13540cbef56f7b48f98aeb2f7bca2
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2020-03-30 02:20:12 -0500

    Crammed the whole thing into one patch...doesn't work yet.

--- YeetDebris/Plugin.cs
+++ YeetDebris/Plugin.cs
@@ -1,21 +1,41 @@
 ﻿using HarmonyLib;
 using IPA;
 using System.Reflection;
-
 namespace YeetDebris
 {
-
-    [Plugin(RuntimeOptions.SingleStartInit)]
+    [Plugin(RuntimeOptions.DynamicInit)]
     public class Plugin
     {
-        internal static Plugin instance { get; private set; }
-        internal static string Name => "YeetDebris";
-
+        const string harmonyID = "com.steven.yeet.debris";
+        Harmony harmony;
+        MethodInfo original;
+        MethodInfo prefix;
         [Init]
         public void Init()
         {
-            instance = this;
-            new Harmony("com.steven.yeet.debris").PatchAll(Assembly.GetExecutingAssembly());
+            original = typeof(NoteDebrisSpawner).GetMethod("SpawnDebris");
+            prefix = typeof(Plugin).GetMethod("Prefix");
         }
+        [OnEnable]
+        public void OnEnable()
+        {
+            harmony = new Harmony(harmonyID);
+            BS_Utils.Utilities.BSEvents.gameSceneLoaded += GameSceneLoaded;
+        }
+        [OnDisable]
+        public void OnDisable()
+        {
+            if (Harmony.HasAnyPatches(harmonyID))
+                harmony.UnpatchAll(harmonyID);
+            BS_Utils.Utilities.BSEvents.gameSceneLoaded -= GameSceneLoaded;
+        }
+        private void GameSceneLoaded()
+        {
+            if (!BS_Utils.Plugin.LevelData.GameplayCoreSceneSetupData.playerSpecificSettings.reduceDebris)
+                harmony.Patch(original, prefix:new HarmonyMethod(prefix));
+            else if (Harmony.HasAnyPatches(harmonyID))
+                harmony.UnpatchAll(harmonyID);
+        }
+        internal static bool Prefix() => false;
     }
 }
