1cf813a54df01bb1b3dd5d2d611aa9ff3f97d6e5
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2020-03-30 01:30:14 -0500

    ez mod ez life

---
+++ YeetDebris/Patches/NoteDebrisSpawner_SpawnDebris.cs
@@ -0,0 +1,11 @@
+﻿using HarmonyLib;
+
+namespace YeetDebris
+{
+    [HarmonyPatch(typeof(NoteDebrisSpawner))]
+    [HarmonyPatch("SpawnDebris")]
+    internal class NoteDebrisSpawner_SpawnDebris
+    {
+        private static bool Prefix() => !BS_Utils.Plugin.LevelData.GameplayCoreSceneSetupData.playerSpecificSettings.reduceDebris;
+    }
+}
--- YeetDebris/Plugin.cs
+++ YeetDebris/Plugin.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using HarmonyLib;
 using IPA;
-using IPA.Config;
-using IPA.Config.Stores;
-using UnityEngine.SceneManagement;
-using UnityEngine;
-using IPALogger = IPA.Logging.Logger;
+using System.Reflection;
 
 namespace YeetDebris
 {
@@ -19,43 +12,10 @@ namespace YeetDebris
         internal static string Name => "YeetDebris";
 
         [Init]
-        /// <summary>
-        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
-        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
-        /// Only use [Init] with one Constructor.
-        /// </summary>
-        public void Init(IPALogger logger)
+        public void Init()
         {
             instance = this;
-            Logger.log = logger;
-            Logger.log.Debug("Logger initialized.");
-        }
-
-        #region BSIPA Config
-        //Uncomment to use BSIPA's config
-        /*
-        [Init]
-        public void InitWithConfig(Config conf)
-        {
-            Configuration.PluginConfig.Instance = conf.Generated<Configuration.PluginConfig>();
-            Logger.log.Debug("Config loaded");
-        }
-        */
-        #endregion
-
-        [OnStart]
-        public void OnApplicationStart()
-        {
-            Logger.log.Debug("OnApplicationStart");
-            new GameObject("YeetDebrisController").AddComponent<YeetDebrisController>();
-
-        }
-
-        [OnExit]
-        public void OnApplicationQuit()
-        {
-            Logger.log.Debug("OnApplicationQuit");
-
+            new Harmony("com.steven.yeet.debris").PatchAll(Assembly.GetExecutingAssembly());
         }
     }
 }
